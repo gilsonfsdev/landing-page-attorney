ee96cbe107fa782c32a52af81b6ac2afc305f987
Gilson Ferreira <gilsonfsdev@gmail.com>
2023-11-27 14:44:42 -0300

    initial config

---
+++ src/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto';
+
+  }
+  
+  h1, h2, h3 {
+    font-family: 'Baloo 2';
+  }
+  `
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  // Product
+
+  'yellow-dark': '#c47f17',
+  'yellow-base': '#dbac2c',
+  'yellow-light': '#f1e9c9',
+
+  'purple-dark': '#4b2995',
+  'purple-base': '#8047f8',
+  'purple-light': '#EBE5F9',
+
+  // Base
+
+  'base-title': '#272221',
+  'base-subtitle': '#403937',
+  'base-text': '#574f4d',
+  'base-label': '#8d8686',
+  'base-hover': '#d7d5d5',
+  'base-button': '#e6e5e5',
+  'base-input': '#ededed',
+  'base-card': '#f3f2f2',
+  'base-background': '#fafafa',
+  'base-white': '#ffffff',
+}
